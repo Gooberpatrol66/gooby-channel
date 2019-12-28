c0db677ecec4c24d2186d8b12045d566af04cec1
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2019-12-28 15:57:19 -0600

    add deadbeef

---
+++ deadbeef.scm
@@ -0,0 +1,24 @@
+ 
+(use-modules (guix packages)
+             (guix download)
+             (guix build-system gnu)
+             (guix licenses))
+(define-public deadbeef
+ (package
+   (name "deadbeef")
+   (version "1.8.2")
+   (source (origin
+             (method url-fetch)
+             (uri (string-append "https://github.com/DeaDBeeF-Player/deadbeef/archive/" version
+                                 ".tar.gz"))
+             (sha256
+              (base32
+               "1wsx62gi1bfd9rx5br2gprq4q3sfp7iqj1inxhhiqjasbm85vg50"))))
+   (build-system gnu-build-system)
+   (synopsis "A music player for *nix-like systems and OS X")
+   (description
+    "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+serves as an example of standard GNU coding practices.  As such, it supports
+command-line arguments, multiple languages, and so on.")
+   (home-page "https://deadbeef.sourceforge.io/")
+   (license gpl2)))
