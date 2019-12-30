f0f2b3e7d0f5e2a11748db9d9a9460df2fc5372f
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2019-12-30 02:52:55 -0600

    deadbeef: add autotools input

--- deadbeef.scm
+++ deadbeef.scm
@@ -3,7 +3,8 @@
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
   #:use-module (guix download)
-  #:use-module (gnu packages gettext))
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages autotools)
   
 (define-public deadbeef
  (package
@@ -18,7 +19,8 @@
                "1wsx62gi1bfd9rx5br2gprq4q3sfp7iqj1inxhhiqjasbm85vg50"))))
    (build-system gnu-build-system)
    (native-inputs
-   `(("gettext" ,gettext-minimal)))
+   `(("gettext" ,gettext-minimal))
+	(("autotools" ,autotools)))
    (synopsis "A music player for *nix-like systems and OS X")
    (description
     "GNU Hello prints the message \"Hello, world!\" and then exits.  It
