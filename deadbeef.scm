41c91600a56fc4f50aee4f679ad8db7da4ce296e
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2020-01-07 17:58:21 -0600

    actually revert because git doesn't work

--- deadbeef.scm
+++ deadbeef.scm
@@ -40,13 +40,7 @@
                "1wsx62gi1bfd9rx5br2gprq4q3sfp7iqj1inxhhiqjasbm85vg50"))))
    (build-system gnu-build-system)
    (arguments
-<<<<<<< HEAD
-    `(#:make-flags '("CFLAGS=$CFLAGS -Wno-error"
-                     "CXXFLAGS=$CXXFLAGS -Wno-error")
-      #:tests? #f))
-=======
-    `(#:tests? #f))
->>>>>>> parent of 49a3ecb... deadbeef: ad wno-error flag
+   `(#:tests? #f))
    (native-inputs
    `(("gettext" ,gettext-minimal)
 	 ("automake" ,automake)
