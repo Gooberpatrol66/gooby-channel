59aadf68f07d0cf5e3fd46ff8ff7cbbf3fec92fb
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2020-01-15 20:52:47 -0600

    deadbeef-archive-reader: add blargg patch

--- deadbeef.scm
+++ deadbeef.scm
@@ -99,7 +99,7 @@ command-line arguments, multiple languages, and so on.")
              (uri (svn-reference
                    (url "https://subversion.assembla.com/svn/deadbeef_vfs_archive_reader/trunk/src")
                    (revision (string->number version))))
-;;	     (patches (search-patches "blah"))
+	     (patches (search-patches "fix-blargg_ok-declaration.patch"))
              (sha256
               (base32
                "1sl3xrrv4chv70yxw70bacch4afp52n28m8rdqfp3nc7fpd6ryrh"))))
