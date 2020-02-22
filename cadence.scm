6c14b8ec614f848cafdf513607d8511c9d0f285e
Nathan Dehnel <ncdehnel@gmail.com>
2020-02-22 04:59:05 -0600

    cadence.scm: fix licenses

--- cadence.scm
+++ cadence.scm
@@ -58,7 +58,7 @@
     (home-page "https://kx.studio/Applications:Cadence")
     (synopsis "Collection of tools useful for audio production")
     (description "Includes Cadence, Cadence-JackMeter, Cadence-JackSettings, Cadence-Logs, Cadence-Render, Cadence-XY Controller, Catarina, Catia, Claudia, Claudia-Launcher")
-    (license license:gpl2)))
+    (license gpl2)))
 
 (define-public a2jmidid
   (package
@@ -96,7 +96,7 @@
     (home-page "https://github.com/linuxaudio/a2jmidid")
     (synopsis "ALSA sequencer to JACK MIDI bridging (for jack2)")
     (description "This project aims to ease the usage of legacy, non JACK enabled applications, in a JACK MIDI enabled system, when using jack2.")
-    (license license:gpl2)))
+    (license gpl2)))
 
 (define-public jack_capture
   (package
@@ -134,7 +134,7 @@
     (home-page "https://github.com/kmatheussen/jack_capture")
     (synopsis "A program for recording soundfiles with jack")
     (description "Features: human-readable filenames, >4GB wav file splitting, sndfile format support, mp3 support, writing to stdout, console meter, connecting to jack ports, unlimited jack ports, automatic buffers, auto-reniced disk thread, better performance than Ardour, can write 256 channels of 32 bit wave at once to a local hard drive")
-    (license license:gpl2)))
+    (license gpl2)))
 
 (define-public ladish
   (package
@@ -178,4 +178,4 @@
     (home-page "http://ladish.org/")
     (synopsis "LADI Session Handler")
     (description "LADI Session Handler or simply ladish is a session management system for JACK applications on GNU/Linux. Its aim is to allow you to have many different audio programs running at once, to save their setup, close them down and then easily reload the setup at some other time. ladish doesn't deal with any kind of audio or MIDI data itself; it just runs programs, deals with saving/loading (arbitrary) data and connects JACK ports together. It can also be used to move entire sessions between computers, or post sessions on the Internet for download.")
-    (license license:gpl2)))
+    (license gpl2)))
