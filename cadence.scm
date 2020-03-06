1999d48d12ad82aa55f3d03a7c9bd970ee67947c
Nathan Dehnel <ncdehnel@gmail.com>
2020-03-05 21:21:45 -0600

    flowcanvas: add graphviz, gtkmm

--- cadence.scm
+++ cadence.scm
@@ -19,6 +19,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages base)
   #:use-module (gnu packages gettext)
+  #:use-module (gnu packages graphviz)
   #:use-module (gnu packages perl))
 
 (define-public cadence
@@ -216,9 +217,9 @@
 ;;             (("/usr/bin/env python") ("/usr/bin/env python2"))	  
 ;;             (("RELEASE = False") ("RELEASE = True"))))))))
       `(#:python ,python-2))
-;;    (inputs
-;;     `(("libsndfile" ,libsndfile)
-;;       ("jack2" ,jack-2)))
+    (inputs
+     `(("graphviz" ,graphviz)
+       ("gtkmm" ,gtkmm-2)))
 ;;       ("dbus" ,dbus)))
     (native-inputs
      `(("pkg-config" ,pkg-config)))
