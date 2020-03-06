1099c1bf13bdbbe4c116ce59e97fdd74178601cf
Nathan Dehnel <ncdehnel@gmail.com>
2020-03-05 23:38:38 -0600

    cadence: try to fix pyqt5

--- cadence.scm
+++ cadence.scm
@@ -46,10 +46,10 @@
          (list (string-append "PREFIX=" (assoc-ref %outputs "out")))
          #:phases
          (modify-phases %standard-phases
-;;         (add-before 'build 'set-CC-variable-and-show-features
-;;           (lambda _
-;;             (setenv "CC" "gcc")
-;;             (invoke "make" "features")))
+          (add-before 'build 'set-CC-variable-and-show-features
+           (lambda _
+             (setenv "CC" "gcc")
+             (invoke "make" "features")))
           (delete 'configure))))
     (inputs
      `(("jack2" ,jack-2)
